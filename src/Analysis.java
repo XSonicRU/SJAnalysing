8eb6f12f19a807a8663cc6df7f89af8455abd556
Gohan <miva-2002@mail.ru>
2016-12-26 15:54:23 +0400

    Digits analysis now working. It was very "hard" again.

--- src/Analysis.java
+++ src/Analysis.java
@@ -33,20 +33,19 @@ public class Analysis {
                     max = buf;
                 }
             }
-
             return new char[]{(char) v.get(v.indexOf(max) - 1), (char) v.get(v.indexOf(1) - 1)}; //First - max, second - min
         } else if (type == 1) {
 
         } else if (type == 2) {
             Vector v = new Vector();
             for (char a : data) {
-				if(Character.isWhitespace(a)){
-					continue;
-				}
+                if (Character.isWhitespace(a)) {
+                    continue;
+                }
                 if (v.contains(a)) {
                     int oldValue = (int) (v.get(v.indexOf(a) + 1));
                     v.remove(v.indexOf(a) + 1);
-                    v.insertElementAt (oldValue +1, v.indexOf(a) + 1);
+                    v.insertElementAt(oldValue + 1, v.indexOf(a) + 1);
                 } else {
                     v.add(a);
                     v.add(1);
@@ -62,7 +61,28 @@ public class Analysis {
 
             return new char[]{(char) v.get(v.indexOf(max) - 1), (char) v.get(v.indexOf(1) - 1)}; //First - max, second - min
         } else { //type == 3
-
+            Vector v = new Vector();
+            for (char a : data) {
+                if (!Character.isDigit(a)) {
+                    continue;
+                }
+                if (v.contains(a)) {
+                    int oldValue = (int) (v.get(v.indexOf(a) + 1));
+                    v.remove(v.indexOf(a) + 1);
+                    v.insertElementAt(oldValue + 1, v.indexOf(a) + 1);
+                } else {
+                    v.add(a);
+                    v.add(1);
+                }
+            }
+            int max = 0;
+            for (int i = 1; i < v.size(); i = i + 2) {
+                int buf = (int) v.get(i);
+                if (buf > max) {
+                    max = buf;
+                }
+            }
+            return new char[]{(char) v.get(v.indexOf(max) - 1), (char) v.get(v.indexOf(1) - 1)}; //First - max, second - min
         }
         return null; //HOTFIX LOL
     }
