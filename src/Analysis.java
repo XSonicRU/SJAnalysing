bf17e8ac1669d87841992b6cf2cbc2997806aadc
Gohan <miva-2002@mail.ru>
2017-01-07 14:55:54 +0400

    2 types of 'all chars' type, some optimizations

--- src/Analysis.java
+++ src/Analysis.java
@@ -5,13 +5,14 @@ public class Analysis {
     private static char[] data;
     private static int type;
 
-    public Analysis(char[] sdata, int stype) { //type: 0 - letters, 1 - words, 2 - all chars, 3 - digits
+    public Analysis(char[] sdata, int stype) { //type: 0 - letters, 1 - words, 2 - all chars(no whitespaces), 3 - digits,4 - all chars(with whitespaces)
         data = sdata;
         type = stype;
     }
 
     public char[] Start() {
-        if (new String(data).trim().equals("")) {
+        data = new String(data).trim().toCharArray();
+        if (new String(data).equals("")) {
             return new char[]{' ', ' '};
         }
         if (type == 0) {
@@ -40,11 +41,9 @@ public class Analysis {
         } else if (type == 1) {
             Vector v = new Vector();
             if (Arrays.binarySearch(data, ' ') == -1) {
-                return data;
+                return new char[]{' ', ' '};
             }
-            if (data[0] == ' ') {
-                data = Arrays.copyOfRange(data, 1, data.length);
-            }
+
         } else if (type == 2) {
             Vector v = new Vector();
             for (char a : data) {
@@ -69,7 +68,7 @@ public class Analysis {
             }
 
             return new char[]{(char) v.get(v.indexOf(max) - 1), (char) v.get(v.indexOf(1) - 1)}; //First - max, second - min
-        } else { //type == 3
+        } else if (type == 3) {
             Vector v = new Vector();
             for (char a : data) {
                 if (!Character.isDigit(a)) {
@@ -92,7 +91,28 @@ public class Analysis {
                 }
             }
             return new char[]{(char) v.get(v.indexOf(max) - 1), (char) v.get(v.indexOf(1) - 1)}; //First - max, second - min
+        } else { //type == 4
+            Vector v = new Vector();
+            for (char a : data) {
+                if (v.contains(a)) {
+                    int oldValue = (int) (v.get(v.indexOf(a) + 1));
+                    v.remove(v.indexOf(a) + 1);
+                    v.insertElementAt(oldValue + 1, v.indexOf(a) + 1);
+                } else {
+                    v.add(a);
+                    v.add(1);
+                }
+            }
+            int max = 0;
+            for (int i = 1; i < v.size(); i = i + 2) {
+                int buf = (int) v.get(i);
+                if (buf > max) {
+                    max = buf;
+                }
+            }
+
+            return new char[]{(char) v.get(v.indexOf(max) - 1), (char) v.get(v.indexOf(1) - 1)}; //First - max, second - min
         }
-        return null; //HOTFIX LOL
+        return null; //useless string
     }
 }
