a8498e2c07a5b130a15dd3e53431fe56b73fac05
Gohan <miva-2002@mail.ru>
2017-01-07 17:17:26 +0400

    Huge optimization, type "words" is almost working, fixing min use bug.

--- src/Analysis.java
+++ src/Analysis.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.Vector;
 
+//TODO: Сделать первый тип до конца рабочим и исправить баг с тем что всегда минимально используемый объект это объект который есть используется один раз.
 public class Analysis {
     private static char[] data;
     private static int type;
@@ -30,9 +31,10 @@ public class Analysis {
                     v.add(1);
                 }
             }
+            int buf;
             int max = 0;
             for (int i = 1; i < v.size(); i = i + 2) {
-                int buf = (int) v.get(i);
+                buf = (int) v.get(i);
                 if (buf > max) {
                     max = buf;
                 }
@@ -40,10 +42,43 @@ public class Analysis {
             return new char[]{(char) v.get(v.indexOf(max) - 1), (char) v.get(v.indexOf(1) - 1)}; //First - max, second - min
         } else if (type == 1) {
             Vector v = new Vector();
+            Vector words = new Vector();
             if (Arrays.binarySearch(data, ' ') == -1) {
                 return new char[]{' ', ' '};
             }
-
+            char cbuf;
+            String sdata = new String(data);
+            String buf = "";
+            for (int i = 0; i < sdata.length(); i++) {
+                cbuf = sdata.charAt(i);
+                if (sdata.charAt(i) == ' ') {
+                    words.add(buf);
+                    buf = "";
+                } else {
+                    buf = buf + cbuf;
+                }
+            }
+            Object obuf;
+            for (int i = 0; i < words.size(); i++) {
+                obuf = words.get(i);
+                if (v.contains(obuf)) {
+                    int ind = v.indexOf(obuf) + 1;
+                    v.remove(ind);
+                    v.insertElementAt(((int) v.get(ind)) + 1, ind);
+                } else {
+                    v.add(obuf);
+                    v.add(1);
+                }
+            }
+            int ibuf;
+            int max = 0;
+            for (int i = 1; i < v.size(); i = i + 2) {
+                ibuf = (int) v.get(i);
+                if (ibuf > max) {
+                    max = ibuf;
+                }
+            }
+            return null; //F*** IT
         } else if (type == 2) {
             Vector v = new Vector();
             for (char a : data) {
@@ -51,17 +86,18 @@ public class Analysis {
                     continue;
                 }
                 if (v.contains(a)) {
-                    int oldValue = (int) (v.get(v.indexOf(a) + 1));
-                    v.remove(v.indexOf(a) + 1);
-                    v.insertElementAt(oldValue + 1, v.indexOf(a) + 1);
+                    int ind = v.indexOf(a) + 1;
+                    v.remove(ind);
+                    v.insertElementAt(((int) (v.get(ind))) + 1, ind);
                 } else {
                     v.add(a);
                     v.add(1);
                 }
             }
+            int buf;
             int max = 0;
             for (int i = 1; i < v.size(); i = i + 2) {
-                int buf = (int) v.get(i);
+                buf = (int) v.get(i);
                 if (buf > max) {
                     max = buf;
                 }
@@ -75,17 +111,18 @@ public class Analysis {
                     continue;
                 }
                 if (v.contains(a)) {
-                    int oldValue = (int) (v.get(v.indexOf(a) + 1));
-                    v.remove(v.indexOf(a) + 1);
-                    v.insertElementAt(oldValue + 1, v.indexOf(a) + 1);
+                    int ind = v.indexOf(a) + 1;
+                    v.remove(ind);
+                    v.insertElementAt(((int) (v.get(ind))) + 1, ind);
                 } else {
                     v.add(a);
                     v.add(1);
                 }
             }
+            int buf;
             int max = 0;
             for (int i = 1; i < v.size(); i = i + 2) {
-                int buf = (int) v.get(i);
+                buf = (int) v.get(i);
                 if (buf > max) {
                     max = buf;
                 }
@@ -95,17 +132,18 @@ public class Analysis {
             Vector v = new Vector();
             for (char a : data) {
                 if (v.contains(a)) {
-                    int oldValue = (int) (v.get(v.indexOf(a) + 1));
-                    v.remove(v.indexOf(a) + 1);
-                    v.insertElementAt(oldValue + 1, v.indexOf(a) + 1);
+                    int ind = v.indexOf(a) + 1;
+                    v.remove(ind);
+                    v.insertElementAt(((int) (v.get(ind))) + 1, ind);
                 } else {
                     v.add(a);
                     v.add(1);
                 }
             }
+            int buf;
             int max = 0;
             for (int i = 1; i < v.size(); i = i + 2) {
-                int buf = (int) v.get(i);
+                buf = (int) v.get(i);
                 if (buf > max) {
                     max = buf;
                 }
@@ -113,6 +151,5 @@ public class Analysis {
 
             return new char[]{(char) v.get(v.indexOf(max) - 1), (char) v.get(v.indexOf(1) - 1)}; //First - max, second - min
         }
-        return null; //useless string
     }
 }
